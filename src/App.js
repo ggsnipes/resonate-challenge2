90320f703521c821b9ffd37995712053bb62d6e9
ggsnipes <31529149+ggsnipes@users.noreply.github.com>
2023-10-31 18:26:04 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Id from './Id.js'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [contacts, setContacts] = useState(null)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(res => setContacts(res))
+  }, [])
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    console.log("this works")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        {contacts ? contacts.map(contact => <a href={contact.id} onClick={handleClick}>{contact.name}</a>) : "now loading contacts..."}
+      </section>
+
+      <section>
+
+      </section>
+
     </div>
   );
 }
---
+++ src/Id.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+/*
+function Id() {
+
+    const [person, setPerson] = useState(null)
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(res => setPerson(res))
+      }, [])
+
+
+    return(     
+        <h2>{person.name}</h2>
+        <h2>{person.username}</h2>
+        <h2>{person.email}</h2>  
+    )
+}
+*/
